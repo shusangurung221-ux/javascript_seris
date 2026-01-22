e150b069374e1d585e5cedf61b40341b4c54dc56
shusangurung221-ux <shusangurung221@gmail.com>
2026-01-22 08:59:14 +0545

    greate number

---
+++ greatethen10.js
@@ -0,0 +1,8 @@
+let arr=[2,34,45,23,4];
+let count=0;
+for(let i of arr){
+    if(i>10){
+        count++;
+    }
+}
+console.log(count);
---
+++ sumofarray.js
@@ -0,0 +1,7 @@
+
+let arr=[1,3,4,5,3,5,3,4];
+let sum=0;
+for(let i of arr){
+    sum=sum+i;
+}
+console.log(sum);
